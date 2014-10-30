14647203425888df426c6cb8c67b6bcbde78ae08
Saif Khan <saifkhan994@gmail.com>
2014-10-29 23:07:59 -0400

    Removed useless migrations. Added name and description property to playlist.

--- db/migrate/20140914004601_add_properties_to_locations.rb
+++
@@ -1,8 +0,0 @@
-class AddPropertiesToLocations < ActiveRecord::Migration
-  def change
-    add_column :locations, :user_id, :integer
-    add_column :locations, :latitude, :decimal
-    add_column :locations, :longitude, :decimal
-    add_column :locations, :recorded_at, :timestamp
-  end
-end
--- db/migrate/20141028042115_create_playlists.rb
+++ db/migrate/20141028042115_create_playlists.rb
@@ -1,7 +1,8 @@
 class CreatePlaylists < ActiveRecord::Migration
   def change
     create_table :playlists do |t|
-
+      t.string :name
+      t.string :description
       t.timestamps
     end
   end
