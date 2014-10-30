c9974ab8a6b6960161a95140fd53afdb386f2ae8
Saif Khan <saifkhan994@gmail.com>
2014-10-29 23:08:56 -0400

    Create for playlist. Added devise authentication so you have to be signed in to make a playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,6 +1,11 @@
 class PlaylistsController < ApplicationController
 
+ # This is our new function that comes before Devise's one
+  before_filter :authenticate_user_from_token!
 
+  # This is Devise's authentication
+  before_filter :authenticate_user!
+  
   skip_before_filter :verify_authenticity_token,
                 :if => Proc.new { |c| c.request.format == 'application/json' }
   
@@ -22,34 +27,26 @@ class PlaylistsController < ApplicationController
     end
 
 	  def create
-	     event = Event.create(params.permit(:name, :description, :start_time, :latitude, :longitude, :address))
-	     friend_array = []
-	     
-	     params[:participants].each do |participant|
-	        friend = User.find(participant['id'])
-	        if(friend) 
-	          friend_array.push(friend)
-	        else
-	           render :status => 400,
-	           :json => { success: false,
-	             friend_id_not_found: friend_id}
-	        end
+	     playlist = Playlist.create(params.permit(:name, :description))
+	     users = playlist.users
+	     if(!users)
+	      	users = []
 	     end
-	     event.users = friend_array.push(current_user)
-
-	     if (event.save)
-	       eventJSON = event.as_json;
-	       participants = event.users;
-	       eventJSON['participants'] = participants
+	     users.push(current_user)
+	     playlist.users = users
+	     if (playlist.save)
+	       playlistJSON = playlist.as_json;
+	       participants = playlist.users;
+	       playlistJSON['users'] = participants
 
 	       render :status => 200,
 	         :json => { success: true,
-	                     event: eventJSON}
+	                     playlist: playlistJSON}
 	        
 	     else
 	         render :status => 400,
 	         :json => { success: false,
-	          error: event.errors}
+	          error: playlist.errors}
 	        
 	     end
 	  end
